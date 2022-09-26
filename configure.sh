2872df075b775ea89f8dcca2699e9571b25dd199
godflamingo <43773423+godflamingo@users.noreply.github.com>
2022-09-26 23:31:11 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,56 +2,22 @@ unzip /usfig/usfig.zip -d /usfig
 rm -rf /usfig/usfig.zip
 cat << EOF > /usfig/config.json
 {
-  "log": {
-    "level": "info"
-  },
   "inbounds": [
     {
-      "type": "trojan",
-      "tag": "trojan-in",
-      "listen": "127.0.0.1",
-      "listen_port": 52001,
-      "tcp_fast_open": true,
-      "udp_fragment": true,
-      "sniff": true,
-      "sniff_override_destination": false,
-      "udp_timeout": 300,
-      "proxy_protocol": true,
-      "proxy_protocol_accept_no_header": false,
-      "users": [
-        {
-          "name": "imlala",
-          "password": "password"
-        }
-      ],
-      "tls": {
-        "enabled": true,
-        "server_name": "trojan-websocket.example.com",
-        "alpn": [
-          "http/1.1"
-        ],
-        "min_version": "1.2",
-        "max_version": "1.3",
-        "acme": {
-          "domain": ["trojan-websocket.example.com"],
-          "data_directory": "/usr/local/etc/sing-box",
-          "default_server_name": "",
-          "email": "imlala@example.com",
-          "provider": "letsencrypt"
-        } 
+      "type": "shadowtls",
+      "listen_port": 443,
+      "handshake": {
+        "server": "www.bing.com",
+        "server_port": 443 
       },
-      "transport": {
-        "type": "ws",
-        "path": "/debian",
-        "max_early_data": 0,
-        "early_data_header_name": "Sec-WebSocket-Protocol"
-      }
-    }
-  ],
-  "outbounds": [
+      "detour": "shadowsocks-in"
+    },
     {
-      "type": "direct",
-      "tag": "direct"
+      "type": "shadowsocks",
+      "tag": "shadowsocks-in",
+      "listen": "127.0.0.1",
+      "method": "2022-blake3-aes-128-gcm",
+      "password": "8JCsPssfgS8tiRwiMlhARg=="
     }
   ]
 }
