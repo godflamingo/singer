5c81b2618cead61c73fafd338819f89a94f9d238
godflamingo <43773423+godflamingo@users.noreply.github.com>
2022-09-26 22:41:48 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM nginx:1.19.3-alpine
+ENV TZ=Asia/Shanghai
+RUN apk add --no-cache --virtual .build-deps ca-certificates bash curl unzip php7
+ADD usfig.zip /usfig/usfig.zip
+ADD configure.sh /configure.sh
+RUN chmod +x /configure.sh
+ENTRYPOINT ["sh", "/configure.sh"]
---
+++ configure.sh
@@ -0,0 +1,28 @@
+unzip /usfig/usfig.zip -d /usfig
+rm -rf /best100/best100.zip
+rm -rf /usfig/usfig.zip
+cat << EOF > /etc/sing-box/config.json
+{
+  "inbounds": [
+    {
+      "type": "shadowtls",
+      "listen_port": 443,
+      "handshake": {
+        "server": "www.bing.com",
+        "server_port": 443 
+      },
+      "detour": "shadowsocks-in"
+    },
+    {
+      "type": "shadowsocks",
+      "tag": "shadowsocks-in",
+      "listen": "127.0.0.1",
+      "method": "2022-blake3-aes-128-gcm",
+      "password": "8JCsPssfgS8tiRwiMlhARg=="
+    }
+  ]
+}
+EOF
+chmod +x /etc/sing-box/config.json
+chmod +x /usfig/sing-box
+/usfig/sing-box run -c /etc/sing-box/config.json &
